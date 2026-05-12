478a25abbe2444f9700fb739ef0cf7c04d120227
Dinstyy <andiniazzahrapuspita0727@gmail.com>
2026-05-12 10:21:46 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+// import Button from 'primevue/button'
+</script>
+
+<template>
+  <HelloWorld />
+</template>
---
+++ src/main.js
@@ -0,0 +1,23 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+// PrimeVue
+import PrimeVue from 'primevue/config'
+import Lara from '@primeuix/themes/lara'
+
+// Icons
+import 'primeicons/primeicons.css'
+
+const app = createApp(App)
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Lara,
+        options: {
+            darkModeSelector: '.my-app-dark'
+        }
+    }
+})
+
+app.mount('#app')
